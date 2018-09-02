4f7ab3e5fcd8ff57a72ae783815759a202f5ba85
alejandro11k <k11alejandro@gmail.com>
2018-09-02 15:05:16 -0300

    ading port msj (PortWorkletNode)

---
+++ public/drums1.js
@@ -0,0 +1,32 @@
+class PortProcessor extends AudioWorkletProcessor {
+    constructor() {
+        super();
+        this._lastUpdate = currentTime;
+        this.port.onmessage = this.handleMessage.bind(this);
+    }
+  
+    handleMessage(event) {
+      console.log('[Processor:Received] "' + event.data.message +
+                  '" (' + event.data.timeStamp + ')');
+    }
+
+    process(inputs, outputs){
+        // Post a message to the node for every 1 second.
+        if (currentTime - this._lastUpdate > 1.0) {
+            this.port.postMessage({
+            message: 'Process is called.',
+            timeStamp: currentTime,
+            });
+            this._lastUpdate = currentTime;
+        }
+        /* process audio */
+        let input = inputs[0];
+        let output = outputs[0];
+        for (let channel = 0; channel < input.length; ++channel){
+            output[channel].set(input[channel]);
+        }
+        return true;
+    }
+}
+
+registerProcessor('port-processor', PortProcessor);

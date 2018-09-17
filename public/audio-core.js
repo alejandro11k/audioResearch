ce0b3859c42730fe0cfe8f53cef4b7b28d8db18d
alejandro11k <k11alejandro@gmail.com>
2018-09-16 22:42:24 -0300

    stop add

--- public/audio-core.js
+++ public/audio-core.js
@@ -1,7 +1,9 @@
 import { PortWorkletNode } from './port-worklet-node.js'
 
+let context = new AudioContext();
+
 export function init() {
-    const context = new AudioContext();
+//    const context = new AudioContext();
         context.audioWorklet.addModule('./processor.js').then(() => {
         let portWorkletNode = new PortWorkletNode(context);
         //let oscillator = new OscillatorNode(context)
@@ -9,6 +11,9 @@ export function init() {
         //oscillator.connect(gainNode)
         //gainNode.connect(portWorkletNode)
         portWorkletNode.connect(context.destination);     
-
     });
+}
+export function stop() {
+    context.close()
+    context = new AudioContext();
 }
--- public/port-worklet-node.js
+++ public/port-worklet-node.js
@@ -13,7 +13,7 @@ export class PortWorkletNode extends AudioWorkletNode {
         this.gainNode = new GainNode(context)
         this.oscillator.connect(this.gainNode)
         this.gainNode.connect(this)
-        this.oscillator.start()
+        //this.oscillator.start()
     }
     handleMessage(event) {
         this.counter++;
@@ -22,7 +22,9 @@ export class PortWorkletNode extends AudioWorkletNode {
         
         // Notify the processor when the node gets 10 messages. Then reset the
         // counter.
-        this.trigger()
+        //this.trigger()
+        this.beep1()
+        //this.kick1()
 
         if (this.counter > 10) {
         this.port.postMessage({
@@ -41,6 +43,36 @@ export class PortWorkletNode extends AudioWorkletNode {
         this.oscillator.frequency.exponentialRampToValueAtTime(0.1, time + 0.5);
         this.gainNode.gain.exponentialRampToValueAtTime(0.1, time + 0.5);
     }
+    kick1 () {
+        this.currentTime = this.context.currentTime
+        console.log(this.currentTime)
+        
+        // prevent cannot call start more that once
+        this.destroyOsc()
+
+        this.oscillator.start(this.currentTime)
+        this.oscillator.frequency.setValueAtTime(180, this.currentTime)
+        this.gainNode.gain.setValueAtTime(0.9, this.currentTime)
+        this.oscillator.frequency.exponentialRampToValueAtTime(0.01, this.currentTime + 0.1)
+        this.gainNode.gain.exponentialRampToValueAtTime(0.01, this.currentTime + 0.1)
+        this.oscillator.stop(this.currentTime + 0.15)
+        
+    }
+    beep1 () {
+        const currentTime = this.context.currentTime
+        // this.gainNode.gain.setValueAtTime(0.3, this.currentTime)
+        // prevent cannot call start more that once
+        this.destroyOsc()
+        this.oscillator.start(currentTime)
+        this.oscillator.stop(currentTime + 0.1)
+    }
+    destroyOsc () {
+        // fix sound glitch
+        this.oscillator = null
+        this.oscillator = new OscillatorNode(this.context)
+        this.oscillator.connect(this.gainNode)
+    }
+
 }
 
 // registerAudioWorkletNode('port-worklet-node', PortWorkletNode)
